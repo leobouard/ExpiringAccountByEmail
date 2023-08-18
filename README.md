c80cefc18256b0fd05c852c1bb2a45094a801297
Léo Bouard <leo.bouard@hotmail.com>
2023-08-18 09:46:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# ExpiringAccountByEmail
+
+This script can be used to inform users and their manager that their Active Directory account is expiring. 
+
+The best use of this PowerShell code is to place it in a scheduled task, executed everyday.
+
+The script accepts several parameters:
+
+- `ExpiresInDays`: Used to indicate when the email should be sent according to the account expiration date. By default, email will be sent 30, 15 and 7 days before the expiration.
+- `Recipients`: The possible values are "Self" and/or "Manager"
+- `TestRecipient`: This parameter can be used when you test the script to make sure that everything is OK. When this parameter is employed, all emails will be sent to theses addresses. The default value is <email@domain.co>.
+- `SearchBase`: Allow to scope the script to a specific organizational unit. The default value is the root of the domain.
+
+Preview of an email:
+
+![preview of an email](preview.png)
---
+++ script.ps1
@@ -0,0 +1,38 @@
+﻿param(
+    [array]$ExpiresInDays = (30,15,7),
+    [Parameter(Mandatory=$true)][ValidateSet('Self','Manager')][array]$Recipients,
+    [array]$TestRecipient = ('email@domain.co'),
+    [string]$SearchBase = (Get-ADDomain).DistinguishedName
+)
+
+$properties = 'AccountExpirationDate','Manager','EmailAddress'
+$users = Get-ADUser -Filter {Enabled -eq $true} -Properties $properties -SearchBase $searchBase |
+    Where-Object {$_.AccountExpirationDate} | Sort-Object -Property Accountexpirationdate
+
+$users | ForEach-Object {
+    $value = [int](New-TimeSpan -End $_.AccountExpirationDate).TotalDays
+    $_ | Add-Member -MemberType NoteProperty -Name 'ExpiresIn' -Value $value -Force
+}
+
+$users | Where-Object {$_.ExpiresIn -in $ExpiresInDays} | ForEach-Object {
+
+    $body = Get-Content -Path $PSScriptRoot\body.html -Encoding UTF8
+    $body = $ExecutionContext.InvokeCommand.ExpandString($body)
+
+    $splat = @{
+        Body       = $body
+        BodyAsHtml = $true
+        Encoding   = 'UTF8'
+        From       = 'noreply@domain.com'
+        SmtpServer = 'smtp.domain.com'
+        Subject    = "[IT] $($_.Name)'s account expires in $($_.ExpiresIn) day(s)"
+    }
+
+    $To = [System.Collections.Generic.List[string]]@()
+    if ($Recipients -contains 'Manager' -and $_.Manager) { $To.Add((Get-ADUser $_.Manager -Properties EmailAddress).EmailAddress) }
+    if ($Recipients -contains 'Self' -and $_.EmailAddress) { $To.Add($_.EmailAddress) }
+    if ($TestRecipient) { $To = $TestRecipient }
+    $splat.Add('To',$To)
+
+    Send-MailMessage @splat
+}
